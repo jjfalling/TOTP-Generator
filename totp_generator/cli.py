4118cf63bc16eb2a1f28fc42485797ed2b05d9e8
Jeremy Falling <projects@falling.se>
2019-03-22 09:17:03 +0100

    fix help text

--- totp_generator/cli.py
+++ totp_generator/cli.py
@@ -121,16 +121,15 @@ def main():
     parser.add_argument('-s', '--service', type=str, default=None, help='specify a TOTP service instead of picking ' +
                                                                         'from a list.')
     parser.add_argument('-v', '--version', action='store_true', help='show version and exit')
-    parser.add_argument('-q', '--quiet', action='store_true', help='does nothing unless combined with another flag: ' +
+    parser.add_argument('-q', '--quiet', action='store_true', help='does nothing unless combined with another flag:\n' +
                                                                    'when used with the copy flag no TOTP code is ' +
-                                                                   'shown but all other output is shown.\n' +
+                                                                   'not shown but all other output is shown.\n' +
                                                                    'when used with the service flag no output other ' +
                                                                    'than the TOTP code is shown.\n' +
                                                                    'when used with the copy and service flags no output'
                                                                    ' is shown at all.\n' +
-                                                                   'overrides the debug flag and does not apply to' +
-                                                                   ' import, export, add, edit, remove, and help ' +
-                                                                   'flags.\n' +
+                                                                   'does not apply to import, export, add, edit, '+
+                                                                   'remove, debug, and help flags.\n' +
                                                                    'errors will always be shown.')
     args = parser.parse_args()
 
